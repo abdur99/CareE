877e2337212ca43d735d9d7f021815caf22a0af0
rayyanali0499 <49107308+rayyanali0499@users.noreply.github.com>
2019-12-04 01:38:09 -0500

    Deleted extra stuff

--- app/src/main/java/com/example/care_e/PreviousTrips.kt
+++ app/src/main/java/com/example/care_e/PreviousTrips.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import java.util.*
 
 
 class PreviousTrips : Fragment() {
@@ -16,7 +18,34 @@ class PreviousTrips : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_car, container, false)
+
+        var homeview = inflater.inflate(R.layout.fragment_home_screen, container, false)
+
+        var GreetingMessage = homeview.findViewById(R.id.GreetingMessage) as TextView
+
+        //Get the time of day
+        val date = Date()
+        val cal = Calendar.getInstance()
+        cal.time = date
+        val hour = cal.get(Calendar.HOUR_OF_DAY)
+
+        //Set greeting
+        var greeting: String? = null
+        if (hour >= 12 && hour < 17) {
+            greeting = "Good Afternoon"
+        } else if (hour >= 17 && hour < 21) {
+            greeting = "Good Evening"
+        } else if ((hour >= 21 && hour < 24) || (hour >= 0 && hour <= 5)) {
+            greeting = "Good Night"
+        } else {
+            greeting = "Good Morning"
+        }
+
+        //set the string being displayed by the TextView to the greeting
+        //message for the friend
+        GreetingMessage.setText("$greeting!")
+
+        return homeview
     }
 
 }
