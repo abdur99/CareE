34c6dd558bcf78bbaf7d903e5a2e6e7b37b8e39d
Abdur <abdura99@gmail.com>
2019-12-08 18:15:42 -0500

    Fixes in Visualization

--- app/src/main/java/com/example/care_e/DataVisualization.kt
+++ app/src/main/java/com/example/care_e/DataVisualization.kt
@@ -27,15 +27,15 @@ private const val ARG_PARAM2 = "param2"
  */
 class DataVisualization : Fragment() {
     // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
+    private var mParam1: Bundle? = null
+    private var mParam2: Bundle? = null
     private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            mParam1 = it.getString(ARG_PARAM1)
-            mParam2 = it.getString(ARG_PARAM2)
+            mParam1 = it.getBundle(ARG_PARAM1)
+            mParam2 = it.getBundle(ARG_PARAM2)
         }
     }
 
